5a0bef8595ae7a484255c62af43d5533ff1b16b8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-07 10:44:05 +0000

    Deploy xg4/turntable to github.com/xg4/turntable.git:gh-pages

--- src.2ae73fac.js
+++ src.2ae73fac.js
@@ -25,4 +25,3 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"react":"1n8/","../../src/index":"WdSo"}],"zo2T":[function(require,module,exports) {
 "use strict";var e=u(require("react")),r=u(require("react-dom")),t=u(require("./App"));function u(e){return e&&e.__esModule?e:{default:e}}r.default.render(e.default.createElement(t.default,null),document.getElementById("root"));
 },{"react":"1n8/","react-dom":"NKHc","./App":"/NHn"}]},{},["zo2T"], null)
-//# sourceMappingURL=/src.93613328.js.map
